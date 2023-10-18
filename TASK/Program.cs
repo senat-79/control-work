2c959979d4bf28048aa56752cbdeff92cc274d85
senat-79 <senat79@gmail.com>
2023-10-18 16:01:11 +0300

    Первый массив

---
+++ TASK/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Написать программу, которая из имеющегося массива строк формирует 
+новый массив из строк, длина которых меньше, либо равна 3 символам.*/
+
+
+void array(string[] stringArray)
+{
+    for(int i = 0; i < stringArray.Length; i ++)
+{
+    Console.WriteLine($"Введите {i+1} элемент массива");
+    stringArray[i] = Console.ReadLine();
+}
+}
